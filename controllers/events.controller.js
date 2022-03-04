eb81c668ea2cea209f9281a35c5ae565a793cf0a
Anthony <Anthony.otoniel@hotmail.com>
2022-03-04 17:04:27 -0400

    backend terminado

--- controllers/events.controller.js
+++ controllers/events.controller.js
@@ -1,8 +1,9 @@
 import Evento from "../models/Evento";
-export const getEventos = (req, res) => {
+export const getEventos = async (req, res) => {
+  const eventos = await Evento.find().populate("user", "name");
   res.json({
     ok: true,
-    msg: "getEventos",
+    eventos,
   });
 };
 export const crearEvento = async (req, res) => {
@@ -22,13 +23,77 @@ export const crearEvento = async (req, res) => {
     });
   }
 };
-export const actualizarEvento = (req, res) => {
-  res.json({
-    ok: true,
-    msg: "actualizarEvento",
-  });
+export const actualizarEvento = async (req, res) => {
+  const eventoId = req.params.id;
+  const { uid } = req;
+  try {
+    const evento = await Evento.findById(eventoId);
+    if (!evento) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Evento no existe por ese id",
+      });
+    }
+    if (evento.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "No tiene permiso para editar este evento",
+      });
+    }
+    const nuevoEvento = {
+      ...req.body,
+      user: uid,
+    };
+
+    const eventoActualizado = await Evento.findByIdAndUpdate(
+      eventoId,
+      nuevoEvento,
+      { new: true }
+    );
+    return res.json({
+      ok: true,
+      evento: eventoActualizado,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
 };
-export const elimarEvento = (req, res) => {
+export const elimarEvento = async (req, res) => {
+  const eventoId = req.params.id;
+  const { uid: user } = req;
+  try {
+    const evento = await Evento.findById(eventoId);
+    if (!evento) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El evento que esta intentando actualizar no existe",
+      });
+    }
+    if (evento.user !== user) {
+      if (evento.user.toString() !== user) {
+        return res.status(401).json({
+          ok: false,
+          msg: "No tiene permiso para editar este evento",
+        });
+      }
+    }
+    const eventoEliminado = await Evento.findByIdAndDelete(eventoId);
+
+    return res.json({
+      ok: true,
+      eventoEliminado,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
   res.json({
     ok: true,
     msg: "elimarEvento",
